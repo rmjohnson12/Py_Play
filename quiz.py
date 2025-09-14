e588a8b2adb790fb0667b6ecf367faf3a4004b89
rmjohnson12 <rmjohnson12@my.waketech.edu>
2025-09-14 15:19:16 -0400

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,16 @@
+import random
+num_1 = random.randint(1, 10)
+
+import random
+num_2 = random.randrange(1, 11)
+
+import random
+num_3 = random.randrange(1,10)
+
+import random
+num_4 = random.randint(1, 11)
+
+print(f"Books: {num_1}")
+print(f"DVDs: {num_2}")
+print(f"Games: {num_3}")
+print(f"FOURTH {num_4}")
